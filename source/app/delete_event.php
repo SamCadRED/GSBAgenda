540a2f73af6da9de5e6b50180b2f5415151070a3
samuel <samuelcadiou@hotmail.fr>
2020-01-21 15:09:22 +0100

    documentation de delete_event.php

--- source/app/delete_event.php
+++ source/app/delete_event.php
@@ -2,9 +2,11 @@
 session_start();
 include_once "../connection/ddb_conn.php";
 
+// récupération des variables de session
 $id_event = $_GET['id'];
 $id_user = $_SESSION['idUser'];
 
+// Verification de l'identifiant utilisateur récupérer en session 
 $sql = 'SELECT `idUser` FROM Events WHERE idEvent = :id_event;';
 $req = $conn->prepare($sql);
 try {
@@ -14,6 +16,7 @@ try {
     die('Erreur : ' . $e->getMessage());
 }
 
+// suppression de l'event si l'utilisateur existe et est lié à cet évenement
 if ($result) {
     $sql = 'DELETE FROM `Events` WHERE `idEvent` = :id_event';
     $req = $conn->prepare($sql);
